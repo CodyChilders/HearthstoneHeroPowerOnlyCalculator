0c285252be6158c56b2c61f7651cbc1407addfb1
Cody Childers <codyachilders@gmail.com>
2018-12-15 14:35:24 -0800

    Added Priest
    Very straightfoward, probably the simplest one yet.

---
+++ HSHeroPowerBattleCalculator/HeroPowerBattleCalculator/Heros/Priest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroPowerBattleCalculator
+{
+    public class Priest : Hero
+    {
+        public const int LesserHealRecovery = 2;
+
+        public Priest()
+        {
+
+        }
+
+        public override int TakeTurn(int currentMana, bool includeFatigue)
+        {
+            if (IsDead)
+                throw new HeroAlreadyDeadException();
+
+            if (includeFatigue)
+            {
+                CalculateFatigue();
+                if (IsDead)
+                {
+                    return 0;
+                }
+            }
+
+            if(currentMana >= HeroPowerCost)
+            {
+                currentHealth = Math.Min(currentHealth + 2, MaxHealth);
+            }
+
+            return 0;
+        }
+    }
+}
