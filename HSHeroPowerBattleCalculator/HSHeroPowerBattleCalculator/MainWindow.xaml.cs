f834f100860c8e555ad893a5dba1295a9561fc2e
Cody Childers <codyachilders@gmail.com>
2019-01-27 16:02:49 -0800

    Relate combobox to hero
    The combo boxes now can create the desired hero, now to write the model to run the simulation and report back to the UI.

--- HSHeroPowerBattleCalculator/HSHeroPowerBattleCalculator/MainWindow.xaml.cs
+++ HSHeroPowerBattleCalculator/HSHeroPowerBattleCalculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HeroPowerBattleCalculator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,45 @@ namespace HSHeroPowerBattleCalculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void P1_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int newIndex = (sender as ComboBox).SelectedIndex;
+            Hero selectedHero = GetHeroFromCombobox(newIndex);
+        }
+
+        private void P2_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int newIndex = (sender as ComboBox).SelectedIndex;
+            Hero selectedHero = GetHeroFromCombobox(newIndex);
+        }
+
+        Hero GetHeroFromCombobox(int index)
+        {
+            switch(index)
+            {
+                case 0:
+                    return new Druid();
+                case 1:
+                    return new Hunter();
+                case 2:
+                    return new Mage();
+                case 3:
+                    return new Paladin();
+                case 4:
+                    return new Priest();
+                case 5:
+                    return new Rogue();
+                case 6:
+                    return new Shaman();
+                case 7:
+                    return new Warlock();
+                case 8:
+                    return new Warrior();
+                default:
+                    throw new IndexOutOfRangeException($"{index} is not in the valid range 0-9.");
+            }
         }
     }
 }
